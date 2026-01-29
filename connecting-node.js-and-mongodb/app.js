7064ea06c7b2c5a33d515272f6490926321f3ec5
Muhammad Raheel <raheel31102004@gmail.com>
2026-01-29 22:07:38 +0500

    integrated ejs template engine and sent data from database to frontend using it

--- connecting-node.js-and-mongodb/app.js
+++ connecting-node.js-and-mongodb/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const { MongoClient } = require('mongodb');
 
 const app = express();
-
+app.set('view engine','ejs');
 const client = new MongoClient(process.env.MONGODB_URL);
 
 app.get('/', async (req, res) => {
@@ -11,9 +11,9 @@ app.get('/', async (req, res) => {
     const db = client.db(process.env.databaseName);
     const collection = db.collection('firstCollection');
 
-    const result = await collection.find().toArray();
-    console.log(result);
-    res.send(result);
+    const data = await collection.find().toArray();
+    console.log(data);
+    res.render('index',{data})
 })
 
 app.listen(process.env.PORT);
