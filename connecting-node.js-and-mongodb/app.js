a6b63b46543a9b18cf9ac3a4dc8e63aa58d43b3a
Muhammad Raheel <raheel31102004@gmail.com>
2026-01-29 21:34:01 +0500

    improved code formatting

--- connecting-node.js-and-mongodb/app.js
+++ connecting-node.js-and-mongodb/app.js
@@ -1,17 +1,17 @@
 require('dotenv').config();
 const express = require('express');
-const {MongoClient} = require('mongodb');
+const { MongoClient } = require('mongodb');
 
 const app = express();
 
 const client = new MongoClient(process.env.MONGODB_URL);
 
-app.get('/',async (req,res)=>{
-await client.connect();
+app.get('/', async (req, res) => {
+    await client.connect();
     const db = client.db(process.env.databaseName);
-    const collection =db.collection('firstCollection');
+    const collection = db.collection('firstCollection');
 
-    const result =await collection.find().toArray();
+    const result = await collection.find().toArray();
     console.log(result);
     res.send(result);
 })
