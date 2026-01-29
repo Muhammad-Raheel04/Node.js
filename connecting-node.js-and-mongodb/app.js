a25ff7376ee9e3c626c0ab5a7b344267ef47a5ce
Muhammad Raheel <raheel31102004@gmail.com>
2026-01-29 20:52:27 +0500

    worked with npm mongodb package to connect locall mongodb database with node.js

---
+++ connecting-node.js-and-mongodb/app.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const express = require('express');
+const {MongoClient} = require('mongodb');
+
+const app = express();
+
+const client = new MongoClient(process.env.MONGODB_URL);
+
+async function dbConnection(){
+    await client.connect();
+    const db = client.db(process.env.databaseName);
+    const collection =db.collection('firstCollection');
+
+    const result =await collection.find().toArray();
+    console.log(result);
+
+}
+dbConnection();
+
+app.listen(process.env.PORT);
