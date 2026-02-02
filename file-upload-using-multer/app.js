2df52eb02c511dc79db70fd682c52f37465759ef
Muhammad Raheel <raheel31102004@gmail.com>
2026-02-02 22:29:04 +0500

    added post request for sending file metadata details in response as json format

--- file-upload-using-multer/app.js
+++ file-upload-using-multer/app.js
@@ -7,6 +7,13 @@ const upload=multer({dest:'./public/data'});
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'public','index.html'));
 })
+app.post('/file/uploads',upload.single('file'),(req,res)=>{
+   res.json({
+    name:req.file.originalname,
+    size:req.file.size,
+    type:req.file.mimetype
+   })
+})
 app.listen(3200,()=>{
     console.log('Server running on http://localhost:3200');
 })
